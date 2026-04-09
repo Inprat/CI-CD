c7f0998bb16422b9710fd7e58d6b1aa26814d191
Inprat <prateekshiva24@gmail.com>
2026-04-09 13:18:02 +0530

    Initial commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,17 @@
 from app.app import app
 
+
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    
+
     assert response.status_code == 200
     assert b"CI/CD Python App Working" in response.data
+
 
 def test_health():
     client = app.test_client()
     response = client.get("/health")
-    
+
     assert response.status_code == 200
     assert response.json["status"] == "ok"
